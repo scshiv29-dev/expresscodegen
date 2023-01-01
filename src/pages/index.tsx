713f7fdd53f469cd955c97a946df42572131d079
Reenav Hemaria <git@SJCEM23>
2023-01-01 14:58:15 +0530

    authPage Style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,22 +9,42 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
  const { authState, signUp, signIn, signOut } = useAuth();
   return (
-    <>
-       {authState.isAuthenticated ? (
+    
+       authState.isAuthenticated ? (
         <>
         <button onClick={signOut}>Sign Out</button>
         {JSON.stringify(authState.data)}
         </>
       ) : (
         <>
-          <button onClick={() => signUp('thelaw2900@gmail.com', 'password')}>
+          <button  >
             Sign Up
           </button>
-          <button onClick={() => signIn('thelaw2900@gmail.com', 'password')}>
+          <button  >
             Sign In
           </button>
+          <div className={styles.main}>
+<input type="checkbox" id="chk" className={styles.chk} aria-hidden={true} required={true} />
+<div className={styles.signup}>
+<form>
+<label htmlFor='chk' aria-hidden={true} className={styles.label}>Sign up</label>
+<input type="text" className={styles.input} name="txt" placeholder="User name" required={true} />
+<input type="email" className={styles.input} name="email" placeholder="Email" required={true} />
+<input type="password" className={styles.input} name="pswd" placeholder="Password" required={true}/>
+<button className={styles.button} onClick={() => signUp('thelaw2900@gmail.com', 'password')}>Sign up</button>
+</form>
+</div>
+<div className={styles.login}>
+<form>
+<label htmlFor='chk' aria-hidden={true} className={styles.label}>Login</label> 
+<input type="email" className={styles.input} name="email" placeholder="Email" required={true} />
+<input type="password" className={styles.input} name="pswd" placeholder="Password" required={true} />
+<button className={styles.button} onClick={() => signIn('thelaw2900@gmail.com', 'password')}>Login</button>
+</form>
+</div>
+</div>
         </>
-      )}
-    </>
+      )
+    
   )
 }
