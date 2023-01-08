2d5ee0fa158df1d9c04a6348159f21a77c1a7507
Reenav Hemaria <git@SJCEM23>
2023-01-08 16:44:27 +0530

    Homepage Delete

--- src/pages/pastes/index.tsx
+++ src/pages/pastes/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getSession } from 'next-auth/react';
 import { getAllUserPastes, supasupabase ,deletePaste} from '../../../lib/supabase';
 import Base from '../../component/Base';
-import { createStyles, Box, Button, UnstyledButton, Text, Container, Anchor, Card, Group, SimpleGrid, Space, Avatar, HoverCard } from '@mantine/core';
+import { createStyles, Box, Button, UnstyledButton, Text, Container, Anchor, Card, Group, SimpleGrid, Space, Avatar, Popover, Stack, HoverCard, Modal, Affix } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { IconEyeCheck, IconEyeOff, IconLock, IconLockOpen, IconSpy, IconSpyOff } from '@tabler/icons';
+import { IconDotsVertical, IconEyeCheck, IconEyeOff, IconLock, IconLockOpen, IconSpy, IconSpyOff, IconTrash, IconCheck, IconX  } from '@tabler/icons';
+import { useHover } from '@mantine/hooks';
 const useStyles = createStyles((theme) => ({
   card: {
     backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
@@ -29,77 +30,106 @@ const useStyles = createStyles((theme) => ({
     '&:hover': {
 
       boxShadow: `0 0.5em 0.5em -0.4em #ffffff`,
-      transform: "translateY(-0.25em)",
+      transform: "translateY(-0.32em)",
     },
   },
 }));
 export default function Paste({pastes,session}:any) {
         const router=useRouter()
         const { classes, theme } = useStyles();
+        const [opened, setOpened] = useState(false);
 const deletePasteHandler=async (id:any)=>{
 const data=await deletePaste(id,session.supabaseAccessToken)
-if(data){
+console.log(data);
+
   router.reload()
+  setOpened(false)
+
 }
-}
-  const items = pastes?.map((paste:any)=>(
-    <UnstyledButton key={paste.id} className={classes.item} onClick={()=>router.push(`/pastes/${paste.id}`)}>
-      <HoverCard>
-        <HoverCard.Target>
-      <Text size="lg" ta={"center"} >
+  const items = pastes?.map((paste:any,index:any)=>(
+  
+    <UnstyledButton   key={paste.id} className={classes.item} style={{cursor:"default"}}>
+
+    
+      <Text  size="lg" ta={"center"} onClick={()=>router.push(`/pastes/${paste.id}`)} style={{cursor:"pointer"}}>
         {paste.title}
       </Text>
-      </HoverCard.Target>
-      <HoverCard.Dropdown>
-      <Group position="center" style={{marginTop:10}}>
-      <HoverCard width={280} shadow="md" >
+        <HoverCard width={index%3 === 2 ? "md": 400} shadow="md" >
         <HoverCard.Target>
+      <div id={index} style={{position:"absolute", right:"0.5em", top:"0.75em"}}>
+          <IconDotsVertical size={32} color="gray" />
+      </div>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+
+      <Group position="center" style={{marginTop:10, zIndex:100}} >
+      <Popover width={'lg'} shadow="md" >
+        <Popover.Target>
         <Button variant="subtle" color="dark">    
       {paste.isViewOnce?
         <IconEyeCheck size={32}  color={"yellow"}/>
         :<IconEyeOff size={32} color={"yellow"} />}
         </Button>
-        </HoverCard.Target>
-        <HoverCard.Dropdown>
+        </Popover.Target>
+        <Popover.Dropdown>
           <Text size="sm">
           {paste.isViewOnce?"View Once Enabled":"View Once Disabled" }
           </Text>
-        </HoverCard.Dropdown>
-      </HoverCard>
+        </Popover.Dropdown>
+      </Popover>
 
-        <HoverCard width={280} shadow="md" >
-        <HoverCard.Target>
+        <Popover width={'md'} shadow="md" >
+        <Popover.Target>
         <Button variant="subtle" color="dark">    
       {paste.isProtected?
       <IconLock size={32}  color={"lime"}/>
       :<IconLockOpen size={32} color={"lime"}/>}
         </Button>
-        </HoverCard.Target>
-        <HoverCard.Dropdown>
+        </Popover.Target>
+        <Popover.Dropdown>
           <Text size="sm">
           {paste.isProtected?"Protected":"Not Protected" }
           </Text>
-        </HoverCard.Dropdown>
-        </HoverCard>
+        </Popover.Dropdown>
+        </Popover>
 
-        <HoverCard width={280} shadow="md" >
-        <HoverCard.Target>
+        <Popover width={'md'} shadow="md" >
+        <Popover.Target>
         <Button variant="subtle" color="dark">    
       {paste.anonymous?
       <IconSpy  size={32}  color="violet"/>
       :<IconSpyOff size={32} color="violet"/>}
         </Button>
-        </HoverCard.Target>
-        <HoverCard.Dropdown>
+        </Popover.Target>
+        <Popover.Dropdown>
           <Text size="sm">
           {paste.anonymous?"Anonymous":"Not Anonymous"}
           </Text>
-        </HoverCard.Dropdown>
-      </HoverCard>
+        </Popover.Dropdown>
+      </Popover>
+
+        <Button variant="subtle" onClick={() => setOpened(true)} color="dark">
+        <IconTrash size={32} color="red" />
+        </Button>
+        <Modal centered opened={opened}  onClose={() => setOpened(false)} title="Delete?">
+          <Stack align="center">
+          <Text size="sm">
+          Are you sure you want to delete this paste? 
+          </Text>
+          <Group position="center">
+          <IconCheck size={32} color="green" onClick={()=>deletePasteHandler(paste.id)}/>
+          <IconX size={32} color="red" onClick={() => setOpened(false)}/>
+          </Group>
+          </Stack>
+        </Modal>
+
+        
       </Group>
       </HoverCard.Dropdown>
       </HoverCard>
+
     </UnstyledButton>
+
   ));
 return (
     <Base >
