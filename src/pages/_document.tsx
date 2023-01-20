5327ca53bf0465b1c678f5113931f53d13a1a1b3
scshiv29-dev <shivchaudhary790@hotmail.com>
2023-01-20 22:03:47 +0530

    sdf

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-<Head>
-  <title>generator express</title>
-</Head>
+      <Head />
       <body>
         <Main />
-
         <NextScript />
       </body>
     </Html>
   )
 }
+
